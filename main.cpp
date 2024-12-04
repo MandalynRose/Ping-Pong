4fe6f8f3c497d4eefdb459503bd79da75dfe20d2
MandalynRose <mandalynrose8@gmail.com>
2024-12-04 08:38:10 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <raylib.h>
-#include <raymath.h>
+#include "PlayerPaddle.h"
+#include "CpuPaddle.h"                                  //for line 117 to work
 using namespace std;
 
 int player_score = 0;
@@ -16,7 +17,7 @@ public:
     int speed_x, speed_y;
     int radius;
    
-    ~Ball (){                                           //Destructor to delete the sounds/texture
+    ~Ball (){                                           //Destructor to delete the sounds/texture (to keep it in memory)
         UnloadSound(scoreSound);
         UnloadSound(cpuSound);
         UnloadTexture(texture_ball);
@@ -53,13 +54,13 @@ public:
         x = GetScreenWidth() / 2;
         y = GetScreenHeight() / 2;
 
-        int speed_choices[2] = { -1, 1 };
+        int speed_choices[2] = {-1, 1};
         speed_x *= speed_choices[GetRandomValue(0, 1)];
         speed_y *= speed_choices[GetRandomValue(0, 1)];
     }
-};
+}; 
 
-class Paddle {                                          //inheritance
+/*class Paddle {                                          //inheritance
 protected:
     void LimitMovement() {
         if (y <= 0) {
@@ -88,7 +89,7 @@ public:
     }
 
     void Update() {
-        if (IsKeyDown(KEY_UP)) {
+        if (IsKeyDown(KEY_UP)) {                            //Keys we used
             y = y - speed;
         }
         if (IsKeyDown(KEY_DOWN)) {
@@ -98,7 +99,7 @@ public:
     }
 };
 
-class CpuPaddle : public Paddle {                           //inheritance
+class CpuPaddle : public Paddle {                           //dervied class
 public:
     void Update(int ball_y) {
         if (y + height / 2 > ball_y) {
@@ -109,14 +110,13 @@ public:
         }
         LimitMovement();
     }
-};
+}; */
 
 Ball ball;
-Paddle player;
+PlayerPaddle player;
 CpuPaddle cpu;
 
-bool Paused = false;
-
+bool Paused = false;                                                    //Global (so functions can use it)
 
 int main() {
     cout << "Starting the game" << endl;
@@ -124,7 +124,7 @@ int main() {
     const int screenHeight = 800;
     InitWindow(screenWidth, screenHeight, "My Pong Game!");
     InitAudioDevice();
-    SetTargetFPS(60);
+    SetTargetFPS(60);                                                   //Frames per second + timing
     ball.radius = 40;
     ball.x = screenWidth / 2;
     ball.y = screenHeight / 2;
@@ -149,13 +149,13 @@ int main() {
     cpu.texture_paddle = LoadTexture("resources/cat_paw_pixel.png");
 
 
-    Texture2D texture = LoadTexture("resources/PinkBackground.png");            //Adding image in, only png images MYCODE
+    Texture2D texture = LoadTexture("resources/PinkBackground.png");            //Adding image in, only png images 
     Sound sound = LoadSound("resources/meow.ogg");                             //Adding sound to ball
       
-    float scaleX = (float)screenWidth / texture.width;    //Depending on Image MYCODE
-    float scaleY = (float)screenHeight / texture.height;  //MYCODE
+    float scaleX = (float)screenWidth / texture.width;                          //Depending on Image 
+    float scaleY = (float)screenHeight / texture.height;                        //Makes the background stay put in middle
 
-    float scale = (scaleX < scaleY) ? scaleX : scaleY;     // MYCODE
+    float scale = (scaleX < scaleY) ? scaleX : scaleY;                          
 
     while (WindowShouldClose() == false) {
         BeginDrawing();
@@ -168,31 +168,31 @@ int main() {
             ball.Update();
             player.Update();
             cpu.Update(ball.y);
-                                                            // Checking for collisions (rotational as well)
+                                                            //Checking for collisions (rotational as well)
             if (CheckCollisionCircleRec({ ball.x - ball.radius, ball.y - ball.radius }, ball.radius, { player.x, player.y, player.width, player.height })) {
                 ball.speed_x *= -1;
-                PlaySound(sound);                        // Playing sound
+                PlaySound(sound);                        //Playing sound
             }
 
             if (CheckCollisionCircleRec({ ball.x - ball.radius, ball.y - ball.radius }, ball.radius, { cpu.x, cpu.y, cpu.width, cpu.height })) {
                 ball.speed_x *= -1;
-                PlaySound(sound);                        // Playing Sound
+                PlaySound(sound);                        //Playing Sound
             }
         }
-        ClearBackground(WHITE);                         // Drawing
+        ClearBackground(WHITE);                         //Drawing
         DrawRectangle(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
 
-        Rectangle sourceRec = { 0.0f, 0.0f, (float)texture.width, (float)texture.height }; //Size of the image MYCODE
-        // Define destination rectangle (screen area where to draw the texture)
-        Rectangle destRec = {                             // MYCODE
-            (screenWidth - texture.width * scale) / 2,   // X position (centered) MYCODE
-            (screenHeight - texture.height * scale) / 2, // Y position (centered) MYCODE
-            texture.width * scale,                       // Scaled width MYCODE
-            texture.height * scale                       // Scaled height MYCODE
+        Rectangle sourceRec = { 0.0f, 0.0f, (float)texture.width, (float)texture.height }; //Size of the image 
+        //Define destination rectangle (screen area where to draw the texture)
+        Rectangle destRec = {                             
+            (screenWidth - texture.width * scale) / 2,   //X position (centered) 
+            (screenHeight - texture.height * scale) / 2, //Y position (centered) 
+            texture.width * scale,                       //Scaled width 
+            texture.height * scale                       //Scaled height 
         };
-        Vector2 origin = { 0,0 };                        // Where origin begins, starting the destination at this point MYCODE
+        Vector2 origin = { 0,0 };                        //Where origin begins, starting the destination at this point 
 
-        DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, WHITE); //MYCODE
+        DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, WHITE);
         ball.Draw();
         cpu.Draw();
         player.Draw();
@@ -203,7 +203,7 @@ int main() {
         }
         EndDrawing();
     }
-    UnloadSound(sound);                                 // Playing sound
+    UnloadSound(sound);                                 //Playing sound
     UnloadTexture(texture);
     UnloadTexture(ball.texture_ball);
     CloseAudioDevice();
